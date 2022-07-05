28247d6211b060412bfcd6b6795ce4165e0d4c74
Cyconi <jonhilde5@gmail.com>
2022-07-05 16:04:00 -0700

    Patched and dixed shit

--- Modules/WorldEX/Ghost.cs
+++ Modules/WorldEX/Ghost.cs
@@ -13,6 +13,8 @@ using VRC.SDK3.Components;
 using VRC.SDKBase;
 using VRC.Udon;
 using VRC.Udon.Common.Interfaces;
+using System.Collections;
+using static EXO_Udon.UdonStuff;
 
 namespace EXO.Modules
 {
@@ -26,7 +28,7 @@ namespace EXO.Modules
             {
                 G_DeityMode = value;
             });
-            new SingleButton(Ghost, "Bring Key", "Bring Me a Key", () =>
+            new SingleButton(Ghost, "Bring Key", "Brings 3 Keys", () =>
             {
                 GameObject flag1 = GameObject.Find("PoliceStation_A/Functions/KeySpawn/Keys/Key");
                 if (flag1)
@@ -50,7 +52,7 @@ namespace EXO.Modules
                     flag3.transform.position = VRCPlayer.field_Internal_Static_VRCPlayer_0.transform.position + new Vector3(0f, 0.3f, 0f);
                 }
             });
-            new SingleButton(Ghost, "Start Match", "Starts The Match", () =>
+            new SingleButton(Ghost, "Start Match", "Force Starts The Match", () =>
             {
                 GameObject flag2 = GameObject.Find("LobbyManager");
                 if (flag2)
@@ -58,7 +60,7 @@ namespace EXO.Modules
                     flag2.GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "Local_ReadyStartGame");
                 }
             });
-            new SingleButton(Ghost, "Craft All Guns", "GUNNNNSSS", () =>
+            new SingleButton(Ghost, "Craft All Guns", "Crafts All Guns", () =>
             {
                 GameObject flag1 = GameObject.Find("PoliceStation_A/Functions/WeaponWorkShops/T1-M1911");
                 if (flag1)
@@ -171,7 +173,7 @@ namespace EXO.Modules
                     flag22.GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "Local_StartCraft");
                 }
             });
-            new SingleButton(Ghost, "Ghost Win", "Ghost Winnnnn!", () =>
+            new SingleButton(Ghost, "Ghost Win", "Makes Ghost Win", () =>
             {
                 GameObject flag2 = GameObject.Find("LobbyManager");
                 if (flag2)
@@ -179,7 +181,7 @@ namespace EXO.Modules
                     flag2.GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "Local_GhostWin");
                 }
             });
-            new SingleButton(Ghost, "Humans Win", "Humans Winnnnn!", () =>
+            new SingleButton(Ghost, "Humans Win", "Makes Humans Win", () =>
             {
                 GameObject flag2 = GameObject.Find("LobbyManager");
                 if (flag2)
@@ -187,22 +189,47 @@ namespace EXO.Modules
                     flag2.GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "Local_HumanWin");
                 }
             });            
-            new SingleButton(Ghost, "Add Less Money", "Gives You Money", () =>
+            new SingleButton(Ghost, "+ Less Money", "Gives Everyone Money", () =>
             {
-                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.Owner, "AltCurrency");
+                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "AltCurrency");
             });
-            new SingleButton(Ghost, "Add Money", "Gives You Money", () =>
+            new SingleButton(Ghost, "+ Money All", "Gives Everyone Money", () =>
             {
-                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.Owner, "OnSuspiciousKill");                
+                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "OnSuspiciousKill");                
             });
-            new SingleButton(Ghost, "Sub Money", "Deletes Your Money", () =>
+            new SingleButton(Ghost, "- Money All", "Deletes Everyones Money", () =>
             {
-                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.Owner, "OnInnocentKill");                                 
+                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "OnInnocentKill");                                 
             });
             new SingleButton(Ghost, "Fuck Ghosts Money", "Makes The Ghost Lose Money", () =>
             {
-                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.Owner, "OnGhostKill");
+                GameObject.Find("GameManager").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "OnGhostKill");
             });
+            new SingleButton(Ghost, "Kill All", "Kills Everyone", () =>
+            {
+                GameObject.Find("DamageSync").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "BackStab");
+                GameObject.Find("DamageSync").GetComponent<UdonBehaviour>().SendCustomNetworkEvent(NetworkEventTarget.All, "BackStabDamage");                
+            });
+            new ToggleButton(Ghost, "Loop All Guns", "Makes All The Guns Shoot On A Loop", "Stops All guns From Shooting", (value) =>
+            {
+                LoopShootState = value;
+                if (value) MelonLoader.MelonCoroutines.Start(LoopShootAllGuns());
+            });
+        }
+        internal static bool LoopShootState;
+        internal static IEnumerator LoopShootAllGuns()
+        {
+            for (; ; )
+            {
+                SendUdonEventsWithName("Local_StartFiring");
+                yield return new WaitForSeconds(5f);
+                SendUdonEventsWithName("Local_EndFiring");
+                SendUdonEventsWithName("Local_StartReloading");
+                SendUdonEventsWithName("Local_FinishReloading");
+                yield return new WaitForSeconds(2f);
+                if (!LoopShootState)
+                    yield break;
+            }
         }
     }
 }
